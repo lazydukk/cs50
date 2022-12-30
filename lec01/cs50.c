52ac14954f56606133d17700b57f8ae3a74486d0
das <114676073+lazydukk@users.noreply.github.com>
2022-12-30 23:30:22 +0000

    updating cs50

--- lec01/cs50.c
+++ lec01/cs50.c
@@ -2,6 +2,7 @@
 #include <cs50.h>
 
 int main(void) {
-     string answer = get_string("What's your name? ");
-     printf("Hello, %s\n", answer);
+     string first_name = get_string("What's your name? ");
+     string second_name = get_string("What's your second name? ");
+     printf("Howdy, %s %s\n", first_name, second_name);
 }
