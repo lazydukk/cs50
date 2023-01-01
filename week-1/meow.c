76925d921491891c80f109f8643a2b3276d76650
das <114676073+lazydukk@users.noreply.github.com>
2023-01-01 18:58:26 +0000

    added mewo.c

---
+++ week-1/meow.c
@@ -0,0 +1,19 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{   
+    // While Loop 
+    int i = 0;
+    while (i < 3)
+    {
+        printf("meow\n");
+        i++;
+    }
+
+    // For Loop
+    for (int j = 0; j < 0; j++)
+    {
+        printf("meow from for loop\n");
+    }
+}
