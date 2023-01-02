740350a648b19f2487e8d10c24648894895ccfe2
das <114676073+lazydukk@users.noreply.github.com>
2023-01-02 18:21:57 +0000

    mario2.c added

---
+++ mario2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <cs50.h>
+
+// Custom functions
+int get_size(void)
+{
+    int n;
+    do 
+    {
+        n = get_int("Size: ");
+    }
+    while (n < 1);
+
+    return n;
+}
+
+void print_grid(int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
+
+
+int main(void) 
+{
+    int n = get_size();
+
+    print_grid(n);
+}
