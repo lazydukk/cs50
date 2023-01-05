1ca3b18e9cef921ede2c2cdcdb8b46e2d08fedb5
das <114676073+lazydukk@users.noreply.github.com>
2023-01-05 20:23:10 +0000

    program infinite cycle fixed && start and end functions are implemented

--- psets/01/population/population.c
+++ psets/01/population/population.c
@@ -1,15 +1,38 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int size(void);
+
 
 int main(void)
 {
     // TODO: Prompt for start size
-
+    size();
     // TODO: Prompt for end size
-
     // TODO: Calculate number of years until we reach threshold
 
     // TODO: Print number of years
 }
 
+// get start number
+
+
+
+int size(void)
+{
+    int start;
+    do
+    {
+        start = get_int("Start size: ");
+    } while (8 >= start);
+
+    int end;
+    do
+    {
+        end = get_int("End size: ");
+    } 
+    while (start > end);
+
+    return start;
+    return end;
+}
