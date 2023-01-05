1e75a761e3bf83dbc98c82c24ec3be1e437df60f
das <114676073+lazydukk@users.noreply.github.com>
2023-01-05 19:40:07 +0000

    added 2 functions @pop.c

--- psets/01/population/population.c
+++ psets/01/population/population.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 int get_start_size(void);
+int get_end_size(void);
 
 int main(void)
 {
@@ -9,12 +10,16 @@ int main(void)
     int i = get_start_size();
 
     // TODO: Prompt for end size
+    int j = get_end_size();
 
     // TODO: Calculate number of years until we reach threshold
 
     // TODO: Print number of years
 }
 
+
+
+// getting starting value 
 int get_start_size(void)
 {
     int i;
@@ -24,5 +29,17 @@ int get_start_size(void)
     }
     while (i <= 9);
     return i;
+}
+
+// getting ending value 
+int get_end_size(void)
+{
+    int j;
+    do 
+    {
+        j = get_int("Ending size: ");
+    }
+    while (j <= get_start_size());
+    return j;
 }
 
