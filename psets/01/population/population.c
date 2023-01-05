6efc53aa079fedf508a495bc989549b6d42667bd
das <114676073+lazydukk@users.noreply.github.com>
2023-01-05 19:28:45 +0000

    added start size @ pop.c

--- psets/01/population/population.c
+++ psets/01/population/population.c
@@ -1,9 +1,12 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int get_start_size(void);
+
 int main(void)
 {
     // TODO: Prompt for start size
+    int i = get_start_size();
 
     // TODO: Prompt for end size
 
@@ -11,3 +14,15 @@ int main(void)
 
     // TODO: Print number of years
 }
+
+int get_start_size(void)
+{
+    int i;
+    do
+    {
+        i = get_int("Starting size: ");
+    }
+    while (i <= 9);
+    return i;
+}
+
