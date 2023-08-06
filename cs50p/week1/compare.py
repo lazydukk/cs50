65b2eea3009920d3c5b056877c690df1cc240884
lazydukk <bytex07@proton.me>
2023-08-06 18:42:06 +0530

    week1/compare.py

---
+++ cs50p/week1/compare.py
@@ -0,0 +1,10 @@
+x = int(input("what's x? "))
+y = int(input("what's y? "))
+
+# if && elif && else conditionals
+if x < y:
+    print("x is less than y")
+elif x > y: 
+    print("x is greater than y")
+else:
+    print("x is equal to y")
