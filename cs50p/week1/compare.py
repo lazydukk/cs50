321c93c61f61bd4e3a8782ce168ac1654fd87a70
lazydukk <bytex07@proton.me>
2023-08-07 18:50:04 +0530

    week1 conditionals: compare.py & grade.py

--- cs50p/week1/compare.py
+++ cs50p/week1/compare.py
@@ -1,10 +1,8 @@
 x = int(input("what's x? "))
 y = int(input("what's y? "))
 
-# if && elif && else conditionals
-if x < y:
-    print("x is less than y")
-elif x > y: 
-    print("x is greater than y")
+# if && elif && else && or && not equal conditionals
+if x < y or x > y or x != y:
+    print("x is not equal to y")
 else:
     print("x is equal to y")
---
+++ cs50p/week1/grade.py
@@ -0,0 +1,13 @@
+score = int(input("Score: "))
+
+# and condition: and is removed to make the code better
+if score >= 90:
+    print("Grade: A")
+elif score >= 80:
+    print("Grade: B")
+elif score >= 70:
+    print("Grade: C")
+elif score >= 60:
+    print("Grade: D")
+else:
+    print("Grade: F")
