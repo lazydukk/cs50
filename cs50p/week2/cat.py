8d4a058e33e004fc06681c82b53ada38863e41a1
lazydukk <bytex07@proton.me>
2023-11-18 15:13:13 +0530

    cat2.py

--- cs50p/week2/cat.py
+++ cs50p/week2/cat.py
@@ -1,4 +1,4 @@
-# While Loops
+# While Loops (3 different methods to implement a while loop)
 i = 3
 while i != 0:
     print("meow")
@@ -15,6 +15,21 @@ while m < 3:
     m += 1
 
 # For Loops 
-
-for i in [0, 1, 2]:
+# range is used to define the range rather than manually typing the values
+for _ in range(3):      
     print("meow")
+
+# the geek way 😂
+print("geekmeow\n" * 3, end="")
+
+
+# paradigms 
+# when to do something again and again expecting something specific 
+while True:
+    x = int(input("What's x? "))
+    if x > 0:
+        break
+
+for _ in range (x):
+    print("meowparadigms")
+
