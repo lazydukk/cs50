168eebfb6149f30108c3888ce12be9489b1dd4f4
lazydukk <bytex07@proton.me>
2023-11-18 15:30:20 +0530

    hogwarts.py

---
+++ cs50p/week2/hogwarts.py
@@ -0,0 +1,9 @@
+fruits = ["Apple", "Banana", "Orange"]
+for fruit in fruits:
+    print(fruit)
+
+
+students = ["Hermione", "Harry", "Ron"]
+
+for i in range(len(students)):
+    print(i + 1, students[i])
