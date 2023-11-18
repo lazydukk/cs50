ebbf4ab84adc8165c749969331610f0d609d4303
lazydukk <bytex07@proton.me>
2023-11-18 16:49:06 +0530

    add mario.py

---
+++ cs50p/week2/hogwarts3.py
@@ -0,0 +1,11 @@
+# list of dictionaries
+# None is a special keyword in python to state something that smthing is null or none
+students = [
+    {"name": "Hermione", "house": "Gryffindor", "patronus": "Otter"},
+    {"name": "Harry", "house": "Gryffindor", "patronus": "Stag"},
+    {"name": "Ron", "house": "Gryffindor", "patronus": "Jack Russell terrier"},
+    {"name": "Draco", "house": "Slytherin", "patronus": None}
+]
+
+for student in students:
+    print(student["name"], student["patronus"], student["house"], sep=", ")
