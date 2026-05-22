69f82ce74c2c579b18649de19029f764f8c55353
lazydukk <bytex07@proton.me>
2026-05-22 10:08:50 +0530

    hello update

--- CS50P/00/hello.py
+++ CS50P/00/hello.py
@@ -1,5 +1,5 @@
 # Ask the user for their name
-name = input("What's Your Name? ")
+name = input("What's Your Name? ")  # Stores he user input in the variable called "name"
 
 print("Hello, " + name)
 print("Welcome,", name)  # Adds a space on it's own?
