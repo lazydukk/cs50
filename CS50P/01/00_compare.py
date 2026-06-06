b9b14aec5bd8079581c2e9ae150e2719f7c38436
lazydukk <bytex07@proton.me>
2026-06-06 21:49:52 +0530

    week 01

---
+++ CS50P/01/00_compare.py
@@ -0,0 +1,9 @@
+x = int(input("What's x?"))
+y = int(input("What's y?"))
+
+if x > y:
+    print("x is greater than y")
+elif x < y:
+    print("y is greather than x")
+else:
+    print("x and y are equal")
---
+++ CS50P/01/01_compare.py
@@ -0,0 +1,7 @@
+x = int(input("What's x?"))
+y = int(input("What's y?"))
+
+if x != y:
+    print("x and y are not equal")
+else:
+    print("x and y are equal")
